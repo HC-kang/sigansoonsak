0aeb09b68d5ca1d7312e19327c0b17e216bd7f30
HC-kang <weston0713@gmail.com>
2021-05-08 23:06:51 +0900

    0508

--- ch10.py
+++ ch10.py
@@ -66,9 +66,109 @@ a.split()
 
 
 import csv
-f = open('/Users/heechankang/projects/pythonworkspace/sigansoonsak/test.csv', 'r', encoding='utf-8')
+f = open('/Users/heechankang/projects/pythonworkspace/sigansoonsak/test.csv', 'r')
 data = csv.reader(f)
 
 for line in data:
     print(line)
 f.close()
+
+
+infile = open('/Users/heechankang/projects/pythonworkspace/sigansoonsak/phones.txt', 'r')
+outfile = open('/Users/heechankang/projects/pythonworkspace/sigansoonsak/temp.txt', 'a')
+
+for line in infile:
+    outfile.write(line)
+
+infile.close()
+outfile.close()
+
+
+##############
+address = open('/Users/heechankang/projects/pythonworkspace/sigansoonsak/address.txt', 'a')
+
+address.write('''Four score and seven years ago, our fathers brought forth upon this continent a new nation, conceived in liberty and dedicated to the proposition that all men are created equal.
+Now we are engaged in a great civil war, testing whether that nation or any nation so conceived and so dedicated can long endure. We are met on a great battlefield of that war.
+We have come to dedicate a portion of that field as a final resting place for those who here gave their lives that this nation might live. It is altogether fitting and proper that we should do this
+But, in a larger sense, we cannot dedicate, we cannot consecrate, we cannot hallow this ground. The brave men, living and dead, who struggled here have consecrated it, far above our poor power
+ to add or detract. 
+The world will little note, nor long remember, what we say here, but it can never forget what they did here.  It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. 
+  It is rather for us to be here dedicated to the great task remaining before us, that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion, that we here highly resolve that these dead shall not have died in vain, that this nation, under God, shall have a new birth of freedom, and that government of the people, by the people, for the people, shall not perish from the earth.
+''')
+address.close()
+###############
+
+texts = open('/Users/heechankang/projects/pythonworkspace/sigansoonsak/address.txt', 'r')
+
+txtlist = []
+for line in texts:
+    txtlist.append(line.split())
+
+texts.close()
+print(txtlist)
+
+texts = []
+for t in txtlist:
+    for i in t:
+        texts.append(i)
+print(texts)
+
+count = {}
+for i in texts:
+    if i in count:
+        count[i] +=1
+    else:
+        count[i] = 1
+print( count)
+
+###############
+
+in_file = open('/Users/heechankang/projects/pythonworkspace/sigansoonsak/address.txt', 'r')
+out_file = open('/Users/heechankang/projects/pythonworkspace/sigansoonsak/count.txt', 'w')
+
+word_dic = {}
+total_counts = 0
+
+for line in in_file:
+    line = line.rstrip()
+    word_list = line.split()
+
+    for word in word_list:
+        word = word.lower()
+        word = word.strip(',')
+        word = word.strip('.')
+
+        if word in word_dic :
+            word_dic[word] += 1
+            total_counts +=1
+        else:
+            word_dic[word] = 1
+            total_counts += 1
+
+result = ''
+for key in sorted(word_dic.keys()):
+    result = key + ' ' + str(word_dic[key]) + '\n'
+    out_file.write(result)
+
+print('총 단어 수 : ', total_counts)
+
+in_file.close()
+out_file.close()
+
+
+#######################
+import csv
+
+# 입력 파일 출력 파일 열기
+infile = open('/Users/heechankang/projects/pythonworkspace/sigansoonsak/weather_input.csv', 'r')
+data = csv.reader(infile)
+count = 0
+sum = 0
+
+for line in data :
+    count += 1
+    sum += float(line[2])
+
+print("강원도 2009년 01월 부터 2019년 09월까지의 총 강수량: ", sum)
+print("강원도 2009년 01월 부터 2019년 09월까지의 평균 강수량: ", sum / count)
+infile.close( )
