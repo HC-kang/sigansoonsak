a415507bdbc86e27b3a083c4656350d1361d8078
HC-kang <weston0713@gmail.com>
2021-05-09 20:38:16 +0900

    commit

---
+++ ch12.py
@@ -0,0 +1,28 @@
+12135000 % (980000 * 0.85 )
+
+473000/1200
+
+
+for i in range(30):
+    for j in range(1000):
+        if ((980000*0.85)*i - 1200*j) == 12135000:
+            print(i, j)
+            break
+print('?')
+
+a = 980000*0.85*15
+b = 1200*300
+c = a+b
+
+d = c-135000
+d
+
+
+for i in range(30):
+    for j in range(1000):
+        if (820000*i + 1400*j) == d:
+            print(i, j)
+            break
+print('?')
+
+820000*15 + 1400*300
